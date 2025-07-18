f5dcf74d156adacddc4cac8eea449c9d341d99d7
Ayush Anil <105576046+ASH-100703@users.noreply.github.com>
2025-07-18 10:58:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,15 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 import os
 
-app = Flask(__name__);
-app.config['SECRET_KEY'] = 'Ash#100703@e-commerce';
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db';
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False;
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Ash#100703@e-commerce'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app);
+db = SQLAlchemy(app)
+class Product(db.Model):
+id = db.Column(db.Integer , primary_key=True)
+name = db.Column(db.String(100), nullable=False)
+price = db.Column(db.Float, nullabe=False)
+
 
